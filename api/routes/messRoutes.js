4e9486f415ad655a5c8539c1f7173851c5d4b172
Wes <munsonwa@gmail.com>
2017-07-01 13:34:22 -0400

    check commit

--- api/routes/messRoutes.js
+++ api/routes/messRoutes.js
@@ -2,9 +2,9 @@ module.exports = function(app){
 	var messanger = require('../controllers/messController');
 	console.log('routessssssss')
 
-	app.route('/messages/:groupId')
-		.get(messanger.get_messages);
-
-	app.route('message/post')
+	app.route('/messages')
+		.get(messanger.get_messages)
 		.post(messanger.post_message);
+	// app.route('message/post')
+	// 	.post(messanger.post_message);
 }
